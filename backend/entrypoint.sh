fd5c55f3d78ed3e36ef172b45bdc8bf6c650e358
mobeenjobbio <muhammad@jobbio.com>
2026-05-25 06:58:42 +0500

    adds docker setup

---
+++ backend/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+echo "Waiting for redis..."
+while ! nc -z $REDIS_HOST $REDIS_PORT; do
+  sleep 0.1
+done
+echo "Redis started"
+
+cd /app
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+exec "$@"
