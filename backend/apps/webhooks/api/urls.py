2c869ddedeeb62d0f55dc3dca5cd1ae87bdaf538
mobeenjobbio <muhammad@jobbio.com>
2026-06-10 21:49:44 +0500

    resolves typo in webhook urls

--- backend/apps/webhooks/api/urls.py
+++ backend/apps/webhooks/api/urls.py
@@ -5,6 +5,6 @@ from .views import WebhookEndpointListCreateView, WebhookEndpointDetailView, Web
 urlpatterns = [
     path("/endpoints", WebhookEndpointListCreateView.as_view(),name="webhook-endpoint-list"),
     path("/endpoints/<str:pk>", WebhookEndpointDetailView.as_view(), name="webhook-endpoint-detail"),
-    path("/endpoints/<ste:pk>/deliveries", WebhookDeliveryListView.as_view(), name="webhook-delivery-list"),
-    path("/deliveries/<ste:pk>/redeliver/", WebhookRedeliverView.as_view(), name="webhook-redeliver"),
+    path("/endpoints/<str:pk>/deliveries", WebhookDeliveryListView.as_view(), name="webhook-delivery-list"),
+    path("/deliveries/<str:pk>/redeliver/", WebhookRedeliverView.as_view(), name="webhook-redeliver"),
 ]
